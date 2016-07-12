af37149ebe7a2bd1118d857aaed1cc37c9c5b132
dudusbarros <joseeduardobarros@gmail.com>
2016-07-12 10:27:54 -0300

    Add new project structure

---
+++ src/js/_.js
@@ -0,0 +1,5 @@
+/*!
+ * Mowe Citodon Project v0.0.1 (http://letsmowe.org/)
+ * Copyright 2013-2016 Mowe Developers
+ * Licensed under MIT (https://github.com/mowekabanas/citodon/blob/master/LICENSE)
+*/
